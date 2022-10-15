2dbaf2f7d5481565468275175166e033315650d4
keinn51 <soogood97@gmail.com>
2022-10-15 20:57:38 +0900

    set game tile

--- main.js
+++ main.js
@@ -5,18 +5,31 @@ const playTime = document.querySelector('.play-time')
 
 const titleCount = 16
 
-Array(titleCount).fill().forEach((_, i) => {
-    const li = document.createElement("li");
-    li.setAttribute('data-index', i);
-    li.classList.add(`list${i}`);
-    container.appendChild(li)
-})
+function createImageTiles () {
+    const tempListArr = [];
+    Array(titleCount).fill().forEach((_, i) => {
+        const li = document.createElement("li");
+        li.setAttribute('data-index', i);
+        li.classList.add(`list${i}`);
+        tempListArr.push(li);
+    })
+    return tempListArr;
+}
 
 function shuffle(array) {
     let index = array.length - 1;
     while (index > 0) {
         const randomIndex = Math.floor(Math.random() * (index + 1));
+        [array[index], array[randomIndex]] = [array[randomIndex], array[index]];
         console.log(randomIndex)
         index--;
     }
+    return array
 }
+
+const tiles = createImageTiles();
+
+shuffle(tiles).forEach(tile=>container.appendChild(tile))
+
+
+
